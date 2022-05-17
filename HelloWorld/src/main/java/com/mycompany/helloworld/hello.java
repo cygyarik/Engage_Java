96400b04157c0f4b39fd4c1fa4e13168788e8c98
cygyarik <itsyarik@gmail.com>
2022-05-16 20:02:01 -0600

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/hello.java
@@ -0,0 +1,14 @@
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author yarik Morales
+ * email: itsyarik@gmail.com
+ * date: 
+ * purpose: 
+ */
+public class hello {
+    public static void main(String [] args){
+        System.out.println("Hello, Yarik!");
+    }
+}
